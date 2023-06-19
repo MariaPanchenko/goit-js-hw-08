c074b08465f6682151d6923011d685e736d040c5
MariaPanchenko <marysa2502@gmail.com>
2023-06-19 14:55:19 +0300

    3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+let formData = {};
+const form = document.querySelector('form');
+const inputValue = localStorage;
+import throttle from 'lodash.throttle';
 
+const deboucedLocalStorageValueSetter = throttle(data => {
+  inputValue.setItem('formData', JSON.stringify(data));
+}, 500);
+
+form.addEventListener('input', function (event) {
+  formData[event.target.name] = event.target.value;
+  deboucedLocalStorageValueSetter(formData);
+});
+
+if (inputValue.getItem('formData')) {
+  formData = JSON.parse(inputValue.getItem('formData'));
+
+  for (let key in formData) {
+    form.elements[key].value = formData[key];
+  }
+  // console.log(formData);
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  form.reset();
+  localStorage.setItem('formData', null);
+  // localStorage.clear();
+  formData = {};
+});
